ebb1e05ac37a37d44ebd7dff6c16e02d39a3b0d3
nickthorpe71 <nickthorpe71@gmail.com>
2020-11-12 18:48:42 -0600

    adds practice for using commandline arguments

---
+++ argv.py
@@ -0,0 +1,5 @@
+# to access commang line arguments
+from sys import argv
+
+for i in range(len(argv)):
+	print(argv[i])
--- scores.py
+++ scores.py
@@ -1,3 +1,6 @@
 scores = [72,73,33,33,55,66,77,88]
 
 print("Average: " + str(sum(scores) / len(scores)))
+
+s = raw_input("type: ")
+print(s.upper())
